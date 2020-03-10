0c31b5510b0e5947f84c943757c4f8d6aaee8124
chris <chrisastaneh@gmail.com>
2020-03-10 00:01:07 -0400

    React Redux Starter Kit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lms-redux
+
+Steps to install and run:
+
+1. nvm use
+2. npm install
+3. npm run start
+
+The server will start on 8080.
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+"use strict"
+
+import React from 'react';
+
+const Home = () => {
+        return(
+            <div className="jumbotron">
+                <h1>Library Management System</h1>
+            </div>
+        );
+}
+
+export default Home;
